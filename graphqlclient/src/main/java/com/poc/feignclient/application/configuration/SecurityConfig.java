6f846b480457858ed094290b125c9502c7a4e656
venkat velicheti <kiranvelicheti@gmail.com>
2023-03-08 09:34:10 -0500

    Aws Cognito security enabled

---
+++ graphqlclient/src/main/java/com/poc/feignclient/application/configuration/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.poc.feignclient.application.configuration;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.cors().and().csrf().disable()
+            .authorizeRequests(expressionInterceptUrlRegistry -> expressionInterceptUrlRegistry
+                .anyRequest().authenticated())
+            .oauth2ResourceServer().jwt();
+        return http.build();
+    }
+}
+
